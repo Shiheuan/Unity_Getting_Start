4a90fb63e9a87ec2cfe438d475f81fdb12851832
Shiheuan <shiheuan@gmail.com>
2018-10-07 01:54:02 +0800

    Balls start to move. Balls will be destroyed when they are invisible.

---
+++ Assets/_Scripts/Cube_Jump_Scripts/Ball.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        this.GetComponent<Rigidbody>().velocity = new Vector3(8.0f, 8.0f, 0.0f);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	private void OnBecameInvisible() {
+		Destroy(this.gameObject);
+	}
+}

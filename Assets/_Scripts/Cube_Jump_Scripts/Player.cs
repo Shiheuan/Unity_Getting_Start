8dbb0380b1ee31eba44f901bdf2246976f74c985
Shiheuan <shiheuan@gmail.com>
2018-10-07 20:57:58 +0800

    Prohibit Cube from jumping in the air.

--- Assets/_Scripts/Cube_Jump_Scripts/Player.cs
+++ Assets/_Scripts/Cube_Jump_Scripts/Player.cs
@@ -5,15 +5,29 @@ using UnityEngine;
 public class Player : MonoBehaviour {
 
 	protected float jump_speed = 5.0f;
+    public bool is_landing = false;
 	// Use this for initialization
 	void Start () {
-		
+        this.is_landing = false;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		if (Input.GetMouseButtonDown(0)) {
-			this.GetComponent<Rigidbody>().velocity = Vector3.up * this.jump_speed;
-		}
+        if (this.is_landing)
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                this.is_landing = false;
+                this.GetComponent<Rigidbody>().velocity = Vector3.up * this.jump_speed;
+            }
+        }   
 	}
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Floor")
+        {
+            this.is_landing = true;
+        }
+    }
 }

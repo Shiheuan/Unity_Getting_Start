244a5a373786152f232e2e1260081e98234ea8fc
Shiheuan <shiheuan@gmail.com>
2018-10-07 21:53:32 +0800

    modify Cube prefab, with new member "height". So Cube can jump to the height we set.

--- Assets/_Scripts/Cube_Jump_Scripts/Player.cs
+++ Assets/_Scripts/Cube_Jump_Scripts/Player.cs
@@ -5,10 +5,12 @@ using UnityEngine;
 public class Player : MonoBehaviour {
 
 	protected float jump_speed = 10.0f;
+    public float jump_height = 3.0f;
     public bool is_landing = false;
 	// Use this for initialization
 	void Start () {
         this.is_landing = false;
+        jump_speed = Mathf.Sqrt(2.0f * Mathf.Abs(Physics.gravity.y) * this.jump_height);
     }
 	
 	// Update is called once per frame
